f2dfeaf18e07b21836a8cf4ae866fc0c57ff5bfc
Pradisharoon <90965355+Pradishan@users.noreply.github.com>
2023-12-08 13:06:35 +0530

    routing setup

--- quizclashfrontend/src/App.js
+++ quizclashfrontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Page404 from './pages/Page404';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import Profile from './pages/Profile';
+import Quizzes from './pages/Quizzes';
+import Leaderboard from './pages/Leaderboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/quizzes" element={<Quizzes />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+
+        <Route path="/*" element={<Page404 />} />
+      </Routes>
     </div>
   );
 }
---
+++ quizclashfrontend/src/pages/Page404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import img from '../assets/Error-404.svg';
+
+export default function Page404() {
+  return (
+      <>
+            <div className="row justify-content-center align-items-center" style={{"height": "100vh"}}>
+                <div className="col-md-4">
+                  <img src={img} alt='error 404' className='fluid'/>
+                </div>
+            </div>
+      </>
+  )
+}
