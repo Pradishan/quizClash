760cf4f110071476ecc1f8e951c07db9557ae0f9
kavithradimani <109245680+kavithradimani@users.noreply.github.com>
2023-12-15 13:33:23 +0530

    practice

--- quizclashfrontend/src/pages/Profile.js
+++ quizclashfrontend/src/pages/Profile.js
@@ -4,6 +4,17 @@ export default function Profile() {
     <>
       <div>Profile</div>
       <h1>test</h1>
+      
+      <div className="card" style={{width: '18rem'}}>
+  <img src="https://th.bing.com/th/id/OIP.EF_rH_CGlInjFiT_D71OjAAAAA?rs=1&pid=ImgDetMain" className="card-img-top" alt="..." />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+      <img id="codepenio" className="rounded-circle" src="https://th.bing.com/th/id/OIP.EF_rH_CGlInjFiT_D71OjAAAAA?rs=1&pid=ImgDetMain" alt="Sri Lanka" target="_blank"></img>
     </>
   );
 }
