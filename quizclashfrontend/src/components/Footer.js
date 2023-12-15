9a89ffccb41d765b2f6b459dd4895ed65a0b3a45
Pradisharoon <90965355+Pradishan@users.noreply.github.com>
2023-12-15 18:30:15 +0530

    footer adjustment

--- quizclashfrontend/src/components/Footer.js
+++ quizclashfrontend/src/components/Footer.js
@@ -8,13 +8,15 @@ export default function Footer ()
             <div className='sticky-bottom' style={ { backgroundColor: '#F3F3F3' } }>
                 <div className='d-flex justify-content-between mx-3'>
                     <div className='p-2'>
-                        <p className='m-0 text-muted' style={{fontSize:'small'}}>Quizclash@info.com</p>
-                        <p className='m-0 text-muted'style={{fontSize:'small'}}>+9401234567</p>
+                        <p className='m-0 text-muted' style={ { fontSize: 'small' } }>Quizclash@info.com</p>
+                        <p className='m-0 text-muted' style={ { fontSize: 'small' } }>+9401234567</p>
                     </div>
-                    <div className='d-flex align-items-center'>
-                        <p className='m-0 text-muted'><a className="" href="/"><img src={ logo } alt='logo' height={ '18rem' } /></a> copyright©  2023</p>
+                    <div className='d-none d-md-block'>
+                        <div className='d-flex align-items-center mt-3'>
+                            <p className='m-0 text-muted'><a className="" href="/"><img src={ logo } alt='logo' height={ '18rem' } /></a> copyright©  2023</p>
+                        </div>
                     </div>
-                    <div className='d-flex align-items-center justify-content-center d-sm-none d-md-block'>
+                    <div className='d-flex align-items-center justify-content-center'>
                         <a href='' className='px-2 text-muted nav-hover'><i class="bi bi-facebook"></i></a>
                         <a href='' className='px-2 text-muted nav-hover'><i class="bi bi-linkedin"></i></a>
                         <a href='' className='px-2 text-muted nav-hover'><i class="bi bi-github"></i></a>
