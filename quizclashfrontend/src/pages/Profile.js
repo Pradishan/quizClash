f4df09ecec1e64853272d06a4e62a8a14e21e67c
kavithradimani <109245680+kavithradimani@users.noreply.github.com>
2023-12-16 01:27:43 +0530

    Profile.js File

--- quizclashfrontend/src/pages/Profile.js
+++ quizclashfrontend/src/pages/Profile.js
@@ -1,21 +1,39 @@
 import React from 'react'
+import '../styles/profile.css'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import proPic from '../assets/pro pic.jpg';
+import starimg from '../assets/circle-star.jpg';
 
 export default function Profile() {
   return (
     <>
-      <div>Profile</div>
-      <h1>test</h1>
-      
-      <div className="card" style={{width: '18rem'}}>
-  <img src="https://th.bing.com/th/id/OIP.EF_rH_CGlInjFiT_D71OjAAAAA?rs=1&pid=ImgDetMain" className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+    <Navbar />
+    <div className="profile-banner">
+          
+          <div className="infocardContainer">
+            <div id="main">
+              <img src={proPic} alt="Profile" />
+            </div>
+            <div id="textbois">
+              <h3>Emil Alicic</h3>
+              <h5>STDUS001</h5>
+              <a href="#">example@gmail.com</a>
 
-      <img id="codepenio" className="rounded-circle" src="https://th.bing.com/th/id/OIP.EF_rH_CGlInjFiT_D71OjAAAAA?rs=1&pid=ImgDetMain" alt="Sri Lanka" target="_blank"></img>
+              <div id="hotlinks">
+                <a href="#">
+                  <img id="codepenio" src={starimg} alt="Sri Lanka" />
+                </a>
+                275 XP
+              </div>
+            </div>
+          </div>
+        </div>
+
+       
+
+        
+   <Footer />
     </>
   );
 }
