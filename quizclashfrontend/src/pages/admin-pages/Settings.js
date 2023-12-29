3a037dea11126b41c2e442e67bde0cc5a8a4ed60
kavithradimani <109245680+kavithradimani@users.noreply.github.com>
2023-12-29 23:24:40 +0530

    Admin-pages(setting.js)

--- quizclashfrontend/src/pages/admin-pages/Settings.js
+++ quizclashfrontend/src/pages/admin-pages/Settings.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import '../../styles/admin-setting.css'
+import proPic from '../../assets/pro pic.jpg';
 
 export default function Settings() {
   return (
-    <div>Settings</div>
+    <div class="container-xl px-4 mt-4">
+    <div class="row">
+
+     {/*Profile picture card*/ }
+          
+        <div class="col-xl-4">
+            <div class="card mb-4 mb-xl-0">
+                <div class="card-header">Profile Picture</div>
+                <div class="card-body text-center">
+                     {/*Profile picture image*/ }
+                    <img class="img-account-profile rounded-circle mb-2" src={ proPic } alt="pro-pic"/>
+                    <div class="mt-3">
+                        <h4><b>John Doe</b></h4>
+                        <p class="text-secondary mb-1"><b>SDT0054</b></p>
+                        
+                    </div>
+                    <br/>
+                    <br/>
+                    <label class="custom-upload"><input type="file" name="upload_file" />Update Profile Picture</label>
+                    
+                </div>
+                <br/>
+          
+            </div>
+        </div>
+        </div>
+       </div>
   )
 }
