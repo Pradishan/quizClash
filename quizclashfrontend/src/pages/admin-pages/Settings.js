787a84d8fc4e40312068df3d3c80e8be1ba03c68
kavithradimani <109245680+kavithradimani@users.noreply.github.com>
2023-12-30 00:27:17 +0530

    Updated setting.js(Change Password)

--- quizclashfrontend/src/pages/admin-pages/Settings.js
+++ quizclashfrontend/src/pages/admin-pages/Settings.js
@@ -70,6 +70,33 @@ export default function Settings() {
             </div>
         </div>
 
+        {/*Security Setting*/ }
+        <div class="col-xl-6">
+  <div class="card mb-4 mb-xl-0">
+    <div class="card-header">Change Password</div>
+    <div class="card-body">
+        <form>
+            
+            <div class="mb-3">
+                <label class="small mb-1" for="currentPassword">Current Password</label>
+                <input class="form-control" id="currentPassword" type="password" placeholder="Enter current password"/>
+            </div>
+            
+            <div class="mb-3">
+                <label class="small mb-1" for="newPassword">New Password</label>
+                <input class="form-control" id="newPassword" type="password" placeholder="Enter new password"/>
+            </div>
+            
+            <div class="mb-3">
+                <label class="small mb-1" for="confirmPassword">Confirm Password</label>
+                <input class="form-control" id="confirmPassword" type="password" placeholder="Confirm new password"/>
+            </div>
+            <button class="btn btn-primary" type="button">Save Changes</button>
+        </form>
+    </div>
+</div>
+</div>
+
         </div>
        </div>
   )
