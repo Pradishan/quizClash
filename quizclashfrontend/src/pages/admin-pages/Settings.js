ed0ee12fdbb71c97d5734542bb88e185b1cf16c9
kavithradimani <109245680+kavithradimani@users.noreply.github.com>
2023-12-30 00:10:36 +0530

    Updated setting.js file

--- quizclashfrontend/src/pages/admin-pages/Settings.js
+++ quizclashfrontend/src/pages/admin-pages/Settings.js
@@ -29,6 +29,47 @@ export default function Settings() {
           
             </div>
         </div>
+
+  {/*Personal information card*/ }
+        <div class="col-xl-8">
+           
+            <div class="card mb-4">
+                <div class="card-header">Account Details</div>
+                <div class="card-body">
+                    <form>
+                         {/*name*/ }
+                        <div class="mb-3">
+                            <label class="small mb-1" for="name">Name</label>
+                            <input class="form-control" id="name" type="text" placeholder="Enter your name" value=""/>
+                        </div>
+
+                         {/*address*/ }
+                        <div class="mb-3">
+                           
+                                <label class="small mb-1" for="address">Address</label>
+                                <input class="form-control" id="address" type="text" placeholder="Enter your address" value=""/>
+                            </div>
+                            
+                        
+                         {/*email*/ }
+                        <div class="mb-3">
+                            <label class="small mb-1" for="inputEmailAddress">Email address</label>
+                            <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" value=""/>
+                        </div>
+                       
+                        {/*phone number*/ }
+                        <div class="mb-3">
+                            <label class="small mb-1" for="phoneNo">Phone Number</label>
+                            <input class="form-control" id="phoneNo" type="text" placeholder="Enter your phone number" value=""/>
+                        </div>
+
+                       {/*SAVE button*/ }
+                        <button class="btn btn-primary" type="button">Save changes</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
         </div>
        </div>
   )
