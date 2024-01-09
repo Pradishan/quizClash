62f067aa6e8b05694be8ebfddbf3445ae9995210
Kavindra Weerasinghe <wdilshankavindra@gmail.com>
2024-01-09 12:40:27 +0530

    Admin Questions

--- quizclashfrontend/src/pages/Admin.js
+++ quizclashfrontend/src/pages/Admin.js
@@ -1,105 +1,151 @@
-import React, { useState, useEffect, lazy } from 'react'
-import '../styles/sidebar.css'
+import React, { useState, useEffect, lazy } from "react";
+import "../styles/sidebar.css";
 
-import logo1 from '../assets/logo.png';
-import logo2 from '../assets/lgogsymbol.png';
+import logo1 from "../assets/logo.png";
+import logo2 from "../assets/lgogsymbol.png";
 
-const Dashboard = lazy(()=> import('./admin-pages/Dashboard'))
-const Questions = lazy(()=> import('./admin-pages/Questions'))
-const Settings = lazy(()=> import('./admin-pages/Settings'))
-const Users = lazy(()=> import('./admin-pages/Users'))
+const Dashboard = lazy(() => import("./admin-pages/Dashboard"));
+const Questions = lazy(() => import("./admin-pages/Questions"));
+const Settings = lazy(() => import("./admin-pages/Settings"));
+const Users = lazy(() => import("./admin-pages/Users"));
 
-
-export default function Admin ()
-{
-  const [ toggle, setToggle ] = useState( false );
-  const [ tab, setTab ] = useState( '1' );
+export default function Admin() {
+  const [toggle, setToggle] = useState(false);
+  const [tab, setTab] = useState("1");
   let sstyle, linkstyle, logo, logostyle;
 
-  const displayContent = () =>
-  {
-    switch ( tab )
-    {
-      case '1':
+  const displayContent = () => {
+    switch (tab) {
+      case "1":
         return <Dashboard />;
-      case '2':
+      case "2":
         return <Questions />;
-      case '3':
+      case "3":
         return <Users />;
-      case '4':
+      case "4":
         return <Settings />;
       default:
         return <Dashboard />;
     }
   };
 
-  useEffect( () =>
-  {
-    function handleResize ()
-    {
-      const isMdScreen = window.matchMedia( '(min-width: 950px)' ).matches;
-      if ( isMdScreen )
-      {
-        setToggle( false );
-      } else
-      {
-        setToggle( true );
+  useEffect(() => {
+    function handleResize() {
+      const isMdScreen = window.matchMedia("(min-width: 950px)").matches;
+      if (isMdScreen) {
+        setToggle(false);
+      } else {
+        setToggle(true);
       }
     }
     handleResize();
-    window.addEventListener( 'resize', handleResize );
-    return () =>
-    {
-      window.removeEventListener( 'resize', handleResize );
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
     };
-  }, [] );
-  if ( toggle )
-  {
+  }, []);
+  if (toggle) {
     sstyle = {
-      width: '6rem',
-      transition: '0.2s ease-out'
-    }
+      width: "6rem",
+      transition: "0.2s ease-out",
+    };
     linkstyle = {
-      display: 'none'
-    }
+      display: "none",
+    };
     logo = logo2;
-    logostyle = 'd-flex flex-column';
-  } else
-  {
+    logostyle = "d-flex flex-column";
+  } else {
     sstyle = {
-      width: '15rem',
-      transition: '0.2s ease-out'
-    }
+      width: "15rem",
+      transition: "0.2s ease-out",
+    };
     linkstyle = {
-      display: 'block'
-    }
+      display: "block",
+    };
     logo = logo1;
-    logostyle = 'd-flex';
+    logostyle = "d-flex";
   }
   return (
     <>
-      <div className='row vh-100'>
-
-        <div className="shadow" style={ sstyle }>
-          <div className={ logostyle } >
-            <a className="" href="/"><img src={ logo } className='ms-3 mt-3 me-1' alt='logo' height={ '32rem' } /></a>
-            <button className="btn ms-1" type="button" onClick={ () => setToggle( !toggle ) } style={{border:'none'}}>
-              <span style={ { fontSize: '32px' } }><i class="bi bi-list"></i></span>
+      <div className="row vh-100 overflow-hidden">
+        <div className="shadow" style={sstyle}>
+          <div className={logostyle}>
+            <a className="" href="/">
+              <img
+                src={logo}
+                className="ms-3 mt-3 me-1"
+                alt="logo"
+                height={"32rem"}
+              />
+            </a>
+            <button
+              className="btn ms-1"
+              type="button"
+              onClick={() => setToggle(!toggle)}
+              style={{ border: "none" }}
+            >
+              <span style={{ fontSize: "32px" }}>
+                <i class="bi bi-list"></i>
+              </span>
             </button>
           </div>
-          <ul className='sidebar-menu d-flex flex-column ms-0 ps-4 pe-3'>
-            <div className={ `${ 'd-flex align-items-center nav-hover my-2 px-1' } rounded ${ ( tab === '1' ? 'bg-gray' : '' ) }` } typeof='button' onClick={ () => setTab( '1' ) }><i className="bi bi-grid-1x2-fill" ></i><span className='ms-3 mt-1 p-0' style={ linkstyle }>Dashboard</span></div>
-            <div className={ `${ 'd-flex align-items-center nav-hover my-2 px-1' } rounded ${ ( tab === '2' ? 'bg-gray' : '' ) }` } typeof='button' onClick={ () => setTab( '2' ) }><i className="bi bi-question-circle-fill" ></i><span className='ms-3 mt-1 p-0' style={ linkstyle }>Questions</span></div>
-            <div className={ `${ 'd-flex align-items-center nav-hover my-2 px-1' } rounded ${ ( tab === '3' ? 'bg-gray' : '' ) }` } typeof='button' onClick={ () => setTab( '3' ) }><i className="bi bi-person-fill" ></i><span className='ms-3 mt-1 p-0' style={ linkstyle }>Users</span></div>
-            <div className={ `${ 'd-flex align-items-center nav-hover my-2 px-1' } rounded ${ ( tab === '4' ? 'bg-gray' : '' ) }` } typeof='button' onClick={ () => setTab( '4' ) }><i className="bi bi-gear-fill" ></i><span className='ms-3 mt-1 p-0' style={ linkstyle }>Settings</span></div>
+          <ul className="sidebar-menu d-flex flex-column ms-0 ps-4 pe-3">
+            <div
+              className={`${"d-flex align-items-center nav-hover my-2 px-1"} rounded ${
+                tab === "1" ? "bg-gray" : ""
+              }`}
+              typeof="button"
+              onClick={() => setTab("1")}
+            >
+              <i className="bi bi-grid-1x2-fill"></i>
+              <span className="ms-3 mt-1 p-0" style={linkstyle}>
+                Dashboard
+              </span>
+            </div>
+            <div
+              className={`${"d-flex align-items-center nav-hover my-2 px-1"} rounded ${
+                tab === "2" ? "bg-gray" : ""
+              }`}
+              typeof="button"
+              onClick={() => setTab("2")}
+            >
+              <i className="bi bi-question-circle-fill"></i>
+              <span className="ms-3 mt-1 p-0" style={linkstyle}>
+                Questions
+              </span>
+            </div>
+            <div
+              className={`${"d-flex align-items-center nav-hover my-2 px-1"} rounded ${
+                tab === "3" ? "bg-gray" : ""
+              }`}
+              typeof="button"
+              onClick={() => setTab("3")}
+            >
+              <i className="bi bi-person-fill"></i>
+              <span className="ms-3 mt-1 p-0" style={linkstyle}>
+                Users
+              </span>
+            </div>
+            <div
+              className={`${"d-flex align-items-center nav-hover my-2 px-1"} rounded ${
+                tab === "4" ? "bg-gray" : ""
+              }`}
+              typeof="button"
+              onClick={() => setTab("4")}
+            >
+              <i className="bi bi-gear-fill"></i>
+              <span className="ms-3 mt-1 p-0" style={linkstyle}>
+                Settings
+              </span>
+            </div>
           </ul>
         </div>
 
-        {/* main conten  */ }
-        <div className='' style={ { flex: '1', backgroundColor: '#f5f5f5' } }>
-          { displayContent() }
+        {/* main conten  */}
+        <div className="" style={{ flex: "1", backgroundColor: "#f5f5f5" }}>
+          {displayContent()}
         </div>
       </div>
     </>
-  )
+  );
 }
--- quizclashfrontend/src/pages/admin-pages/Settings.js
+++ quizclashfrontend/src/pages/admin-pages/Settings.js
@@ -1,122 +1,184 @@
-import React from 'react'
-import '../../styles/admin-setting.css'
-import proPic from '../../assets/pro pic.jpg';
+import React from "react";
+import "../../styles/admin-setting.css";
+import proPic from "../../assets/pro pic.jpg";
 
 export default function Settings() {
   return (
-    <div class="container-xl px-4 mt-4">
-    <div class="row">
+    <div class="container-xl px-4 vh-100">
+      <div class="row py-2 overflow-y-scroll h-100">
+        {/*Profile picture card*/}
 
-     {/*Profile picture card*/ }
-          
         <div class="col-xl-4">
-            <div class="card mb-4 mb-xl-0">
-                <div class="card-header">Profile Picture</div>
-                <div class="card-body text-center">
-                    <br></br>
-                     {/*Profile picture image*/ }
-                    <img class="img-account-profile rounded-circle mb-2" src={ proPic } alt="pro-pic"/>
-                    <div class="mt-3">
-                        <h4><b>John Doe</b></h4>
-                        <p class="text-secondary mb-1"><b>SDT0054</b></p>
-                        
-                    </div>
-                    <br/>
-                    <br/>
-                  
-                    <label class="custom-upload"><input type="file" name="upload_file" />Update Profile Picture</label>
-                    
-                </div>
-             
-          
+          <div class="card mb-4 mb-xl-0">
+            <div class="card-header">Profile Picture</div>
+            <div class="card-body text-center">
+              <br></br>
+              {/*Profile picture image*/}
+              <img
+                class="img-account-profile rounded-circle mb-2"
+                src={proPic}
+                alt="pro-pic"
+              />
+              <div class="mt-3">
+                <h4>
+                  <b>John Doe</b>
+                </h4>
+                <p class="text-secondary mb-1">
+                  <b>SDT0054</b>
+                </p>
+              </div>
+              <br />
+              <br />
+
+              <label class="custom-upload">
+                <input type="file" name="upload_file" />
+                Update Profile Picture
+              </label>
             </div>
+          </div>
         </div>
 
-  {/*Personal information card*/ }
+        {/*Personal information card*/}
         <div class="col-xl-8">
-           
-            <div class="card mb-4">
-                <div class="card-header">Account Details</div>
-                <div class="card-body">
-                    <form>
-                         {/*name*/ }
-                        <div class="mb-3">
-                            <label class="small mb-1" for="name"><b>Name</b></label>
-                            <input class="form-control" id="name" type="text" placeholder="Enter your name" value=""/>
-                        </div>
-
-                         {/*address*/ }
-                        <div class="mb-3">
-                           
-                                <label class="small mb-1" for="address"><b>Address</b></label>
-                                <input class="form-control" id="address" type="text" placeholder="Enter your address" value=""/>
-                            </div>
-                            
-                        
-                         {/*email*/ }
-                        <div class="mb-3">
-                            <label class="small mb-1" for="inputEmailAddress"><b>Email address</b></label>
-                            <input class="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" value=""/>
-                        </div>
-                       
-                        {/*phone number*/ }
-                        <div class="mb-3">
-                            <label class="small mb-1" for="phoneNo"><b>Phone Number</b></label>
-                            <input class="form-control" id="phoneNo" type="text" placeholder="Enter your phone number" value=""/>
-                        </div>
-
-                       {/*SAVE button*/ }
-                        <button class="btn btn-primary" type="button">Save changes</button>
-                    </form>
+          <div class="card mb-4">
+            <div class="card-header">Account Details</div>
+            <div class="card-body">
+              <form>
+                {/*name*/}
+                <div class="mb-3">
+                  <label class="small mb-1" for="name">
+                    <b>Name</b>
+                  </label>
+                  <input
+                    class="form-control"
+                    id="name"
+                    type="text"
+                    placeholder="Enter your name"
+                    value=""
+                  />
                 </div>
+
+                {/*address*/}
+                <div class="mb-3">
+                  <label class="small mb-1" for="address">
+                    <b>Address</b>
+                  </label>
+                  <input
+                    class="form-control"
+                    id="address"
+                    type="text"
+                    placeholder="Enter your address"
+                    value=""
+                  />
+                </div>
+
+                {/*email*/}
+                <div class="mb-3">
+                  <label class="small mb-1" for="inputEmailAddress">
+                    <b>Email address</b>
+                  </label>
+                  <input
+                    class="form-control"
+                    id="inputEmailAddress"
+                    type="email"
+                    placeholder="Enter your email address"
+                    value=""
+                  />
+                </div>
+
+                {/*phone number*/}
+                <div class="mb-3">
+                  <label class="small mb-1" for="phoneNo">
+                    <b>Phone Number</b>
+                  </label>
+                  <input
+                    class="form-control"
+                    id="phoneNo"
+                    type="text"
+                    placeholder="Enter your phone number"
+                    value=""
+                  />
+                </div>
+
+                {/*SAVE button*/}
+                <button class="btn btn-primary" type="button">
+                  Save changes
+                </button>
+              </form>
             </div>
+          </div>
         </div>
 
-        {/*Security Setting*/ }
+        {/*Security Setting*/}
         <div class="col-xl-6">
-  <div class="card mb-4 mb-xl-0">
-    <div class="card-header">Change Password</div>
-    <div class="card-body">
-        <form>
-            
-            <div class="mb-3">
-                <label class="small mb-1" for="currentPassword"><b>Current Password</b></label>
-                <input class="form-control" id="currentPassword" type="password" placeholder="Enter current password"/>
-            </div>
-            
-            <div class="mb-3">
-                <label class="small mb-1" for="newPassword"><b>New Password</b></label>
-                <input class="form-control" id="newPassword" type="password" placeholder="Enter new password"/>
-            </div>
-            
-            <div class="mb-3">
-                <label class="small mb-1" for="confirmPassword"><b>Confirm Password</b></label>
-                <input class="form-control" id="confirmPassword" type="password" placeholder="Confirm new password"/>
-            </div>
-            <button class="btn btn-primary" type="button">Save Changes</button>
-        </form>
-    </div>
-</div>
-</div>
+          <div class="card mb-4 mb-xl-0">
+            <div class="card-header">Change Password</div>
+            <div class="card-body">
+              <form>
+                <div class="mb-3">
+                  <label class="small mb-1" for="currentPassword">
+                    <b>Current Password</b>
+                  </label>
+                  <input
+                    class="form-control"
+                    id="currentPassword"
+                    type="password"
+                    placeholder="Enter current password"
+                  />
+                </div>
 
+                <div class="mb-3">
+                  <label class="small mb-1" for="newPassword">
+                    <b>New Password</b>
+                  </label>
+                  <input
+                    class="form-control"
+                    id="newPassword"
+                    type="password"
+                    placeholder="Enter new password"
+                  />
+                </div>
 
-{/*Delete account*/ }
-<div class="col-xl-6">
- 
-  <div class="card mb-4">
-    <div class="card-header">Delete Account</div>
-    <div class="card-body">
-        <br/>
-        <p>Deleting your account is a permanent action and cannot be undone. If you are sure you want to delete your account, select the button below.</p>
-        <br/>
-        <button class="btn btn-danger" type="button">I understand, delete my account</button>
-        <br></br>
-    </div>
-    <br></br>
-</div>
-
-</div>
+                <div class="mb-3">
+                  <label class="small mb-1" for="confirmPassword">
+                    <b>Confirm Password</b>
+                  </label>
+                  <input
+                    class="form-control"
+                    id="confirmPassword"
+                    type="password"
+                    placeholder="Confirm new password"
+                  />
+                </div>
+                <button class="btn btn-primary" type="button">
+                  Save Changes
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-       </div>
-  )
+
+        {/*Delete account*/}
+        <div class="col-xl-6">
+          <div class="card mb-4">
+            <div class="card-header">Delete Account</div>
+            <div class="card-body">
+              <br />
+              <p>
+                Deleting your account is a permanent action and cannot be
+                undone. If you are sure you want to delete your account, select
+                the button below.
+              </p>
+              <br />
+              <button class="btn btn-danger" type="button">
+                I understand, delete my account
+              </button>
+              <br></br>
+            </div>
+            <br></br>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
