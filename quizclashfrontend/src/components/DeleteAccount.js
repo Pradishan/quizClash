beca5dcea4ca3c7ee528615bcfaf45bc47c69820
kavithradimani <109245680+kavithradimani@users.noreply.github.com>
2024-01-29 00:59:27 +0530

    DeleteAccount.js File

--- quizclashfrontend/src/components/DeleteAccount.js
+++ quizclashfrontend/src/components/DeleteAccount.js
@@ -1,28 +1,43 @@
 import React from 'react'
+import axios from 'axios';
 import DeleteConfirmationModal from './DeleteConfirmationModal'
 
 export default function DeleteAccount() {
-  return (
-    <div class="col-xl-6">
-          <div class="card mb-4">
-            <div class="card-header">Delete Account</div>
-            <div class="card-body">
-              <br />
-              <p>
-                Deleting your account is a permanent action and cannot be
-                undone. If you are sure you want to delete your account, select
-                the button below.
-              </p>
-              <br />
+  const handleDeleteAccount = async () => {
+    try {
+       
+        const response = await axios.post('/api/delete-account/');
 
-              <DeleteConfirmationModal/>
-             {/* <button class="btn btn-danger" type="button">
-                I understand, delete my account
-              </button>
-*/}  <br></br>
+        
+        alert('Account deleted successfully');
+    } catch (error) {
+        
+        alert('Error deleting account');
+    }
+};
+
+  return (
+    <div className="col-xl-6">
+            <div className="card mb-4">
+                <div className="card-header">Delete Account</div>
+                <div className="card-body">
+                    <br />
+                    <p>
+                        Deleting your account is a permanent action and cannot be
+                        undone. If you are sure you want to delete your account, select
+                        the button below.
+                    </p>
+                    <br />
+
+              {/*<DeleteConfirmationModal/>*/}
+
+              <button className="btn btn-danger" type="button" onClick={handleDeleteAccount}>
+                       Delete Account
+                    </button>
+                    <br></br>
+                </div>
+                <br></br>
             </div>
-            <br></br>
-          </div>
         </div>
-  )
+    );
 }
