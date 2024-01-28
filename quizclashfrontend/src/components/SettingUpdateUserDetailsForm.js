93eb721612192b4f0574f4701a362ac4c502b216
kavithradimani <109245680+kavithradimani@users.noreply.github.com>
2024-01-28 07:22:58 +0530

    Setting Part

--- quizclashfrontend/src/components/SettingUpdateUserDetailsForm.js
+++ quizclashfrontend/src/components/SettingUpdateUserDetailsForm.js
@@ -1,23 +1,117 @@
-import React from 'react'
+import axios from "axios";
+import React, {lazy,useState} from 'react';
+import { toast } from "react-toastify";
+import tostDefault from "../data/tostDefault";
+import { getUserID } from "../util/Authentication";
 
-export default function SettingUpdateUserDetailsForm() {
+const LodingSpinner = lazy(() => import('./LodingSpinner'))
+
+export default function SettingUpdateUserDetailsForm({user}) {
+
+  const [formData, setFormData] = useState({
+    "username": user?.username,
+    "address": user?.address,
+    "email": user?.email,
+    "phone": user?.phone,
+   
+  })
+
+  const [loading, setLoading] = useState(false)
+
+  const handlechange = ({ currentTarget: input }) => {
+    let newData = { ...formData };
+    newData[input.name] = input.value;
+    setFormData(newData);
+  }
+
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    const id = toast.loading("Please wait...", tostDefault);
+    setLoading(true)
+    await axios
+      .patchForm(`accounts/update-user/${getUserID()}`, formData)
+      .then((response) => {
+        // console.log(response.data)
+        if (response.status === 200) {
+
+          toast.update(id, {
+            ...tostDefault,
+            render: "saved",
+            type: "success",
+            isLoading: false,
+           // closeButton: true,
+          });
+        }
+        setLoading(false)
+      })
+      .catch((error) => {
+        if (error?.response?.status === 400) {
+          toast.update(id, {
+            ...tostDefault,
+            render: "Invaild input",
+            type: "error",
+            isLoading: false,
+            closeButton: true,
+          });
+        } else if (error?.code === "ERR_NETWORK") {
+          toast.update(id, {
+            ...tostDefault,
+            render: "Network Error",
+            type: "error",
+            isLoading: false,
+            closeButton: true,
+          });
+        } else {
+          toast.update(id, {
+            ...tostDefault,
+            render: "Something went wrong",
+            type: "error",
+            isLoading: false,
+            closeButton: true,
+          });
+        }
+
+        console.log({
+          message: error.message,
+          code: error.code,
+          response: error.response?.data
+        });
+
+        setLoading(false)
+
+      })
+      .finally(() =>{
+        setLoading(false)
+      });
+  };
+
+  // if data is fetching show loading spinner for user
+  if (loading) return;
+
+
   return (
     <div class="col-xl-8">
     <div class="card mb-4">
       <div class="card-header">Account Details</div>
       <div class="card-body">
-        <form>
+        <form onSubmit={(e) => (submitForm(e))}>
           {/*name*/}
           <div class="mb-3">
-            <label class="small mb-1" for="name">
-              <b>Name</b>
+            <label class="small mb-1" for="editName">
+              <b>Username</b>
             </label>
             <input
               class="form-control"
-              id="name"
+              id="editName"
               type="text"
               placeholder="Enter your name"
-              value=""
+              name="username"
+              onChange={(e) => {
+                handlechange(e);
+              }}
+              value={formData.username}
+              readOnly
             />
           </div>
 
@@ -31,21 +125,31 @@ export default function SettingUpdateUserDetailsForm() {
               id="address"
               type="text"
               placeholder="Enter your address"
-              value=""
+              name="address"
+                        onChange={(e) => {
+                          handlechange(e);
+                        }}
+                        value={formData.address}
+                        required
             />
           </div>
 
           {/*email*/}
           <div class="mb-3">
-            <label class="small mb-1" for="inputEmailAddress">
+            <label class="small mb-1" for="emailaddress">
               <b>Email address</b>
             </label>
             <input
               class="form-control"
-              id="inputEmailAddress"
+              id="emailaddress"
               type="email"
               placeholder="Enter your email address"
-              value=""
+              name="email"
+              onChange={(e) => {
+                handlechange(e);
+              }}
+              value={formData.email}
+              readOnly
             />
           </div>
 
@@ -59,7 +163,12 @@ export default function SettingUpdateUserDetailsForm() {
               id="phoneNo"
               type="text"
               placeholder="Enter your phone number"
-              value=""
+              name="phone"
+              onChange={(e) => {
+                handlechange(e);
+              }}
+              value={formData.phone}
+              required
             />
           </div>
 
@@ -71,5 +180,5 @@ export default function SettingUpdateUserDetailsForm() {
       </div>
     </div>
   </div>
-  )
-}
+  )}
+  
